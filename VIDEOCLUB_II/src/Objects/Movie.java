c5e93879873dc2f43bd3461ea216c6012c6a30d7
Jonathan Guevara <developdantech@gmail.com>
2023-07-07 19:23:58 -0500

    clases

---
+++ VIDEOCLUB_II/src/Objects/Movie.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class Movie {
+   private String title;
+   private String genre;
+   private int year;
+   private Time duration; 
+
+    public Movie(String title, String genre, int year, String duration) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+        this.duration = calculateDuration(duration);
+    }
+    private Time calculateDuration (String duration){
+        String inputFormat = "HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(inputFormat);
+        try {
+            Date date = sdf.parse(duration);
+            Time time = new Time(date.getTime());
+            return time;
+        } catch (ParseException e){
+            e.printStackTrace();
+            return null;
+        }
+    } 
+    public String getTitle(){
+        return this.title;
+    }
+    public String getGenre(){
+        return this.genre;
+    }
+    public int gerYear(){
+        return this.year;
+    }
+    public Time getDuration(){
+        return this.duration;
+    }
+}
---
+++ VIDEOCLUB_II/src/Objects/Rent.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+/**
+ *
+ * @author User
+ */
+public class Rent {
+    public Movie movie;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double price;
+    private int dailyPrice = 10;
+    private int defaultDays = 3;
+    
+    public Rent(Movie movie, String startDate, String endDate){
+        this.movie = movie;
+        this.startDate = calculateDate(startDate);
+        this.endDate = calculateDate(endDate);
+        this.price = calculatePrice();
+    }
+    
+    private double calculatePrice(){
+        long days = ChronoUnit.DAYS.between(this.startDate, this.endDate);
+        double price = 0;
+        if(days <= this.defaultDays){
+            price = this.dailyPrice;
+            return price;
+        }else{
+            for(int i = 1; i<= (days - this.defaultDays); i++){
+                if(i == 1){
+                    price = this.dailyPrice;
+                }
+                price += price * 0.1;
+            }
+            return price;
+        }
+    }
+    
+    private LocalDate calculateDate(String dateToConvert){
+        try{
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+            LocalDate date = LocalDate.parse(dateToConvert, formatter);
+            return date;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public long getDays(){
+        return ChronoUnit.DAYS.between(this.startDate, this.endDate);
+    }
+    public LocalDate getStartDate(){
+        return this.startDate;
+    }
+    public LocalDate getEndDate(){
+        return this.endDate;
+    }
+}
